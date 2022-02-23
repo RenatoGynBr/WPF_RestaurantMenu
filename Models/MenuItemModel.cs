d679d7b93541c69752a5087914687450cfd6770e
Renato Cavalcante <renato.silva@scanbrazilconsulting.com>
2022-02-23 17:43:20 -0300

    Add project files.

---
+++ Models/MenuItemModel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace WPF_RestaurantMenu.Models
+{
+    public class MenuItemModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public MenuItemModel(int id, string name, bool active, decimal price)
+        {
+            try
+            {
+                IdMenuItem = id;
+                Name = name;
+                Active = active;
+                Price = price;
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private int _idMenuItem;
+        private string _name { get; set; }
+        private decimal _price { get; set; }
+        private bool _active { get; set; }
+
+        public int IdMenuItem
+        {
+            get { return _idMenuItem; }
+            set
+            {
+                if (_idMenuItem != value)
+                {
+                    _idMenuItem = value;
+                    OnPropertyChanged("IdMenuItem");
+                }
+            }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+        public decimal Price
+        {
+            get { return _price; }
+            set
+            {
+                if (_price != value)
+                {
+                    _price = value;
+                    OnPropertyChanged("Price");
+                }
+            }
+        }
+        public bool Active
+        {
+            get { return _active; }
+            set
+            {
+                if (_active != value)
+                {
+                    _active = value;
+                    OnPropertyChanged("Active");
+                }
+            }
+        }
+    }
+}
---
+++ ViewModels/MenuItemViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using WPF_RestaurantMenu.Models;
+
+namespace WPF_RestaurantMenu.ViewModels
+{
+    public class MenuItemViewModel : ObservableCollection<MenuItemModel>
+    {
+        public MenuItemViewModel()
+        {
+            FillMenuItemCollection();
+        }
+
+        private void FillMenuItemCollection()
+        {
+            var m1 = new MenuItemModel(1, "Coke", true, 8);
+            Add(m1);
+            var m2 = new MenuItemModel(2, "Diet Coke", true, 8);
+            Add(m2);
+            var m3 = new MenuItemModel(3, "Fries", true, 17);
+            Add(m3);
+            var m4 = new MenuItemModel(4, "Big Burger", true, 29);
+            Add(m4);
+            var m5 = new MenuItemModel(5, "Salad", true, 22);
+            Add(m5);
+            var m6 = new MenuItemModel(6, "Deserts", false, 19);
+            Add(m6);
+            var m7 = new MenuItemModel(7, "Coffee Cream", true, 14);
+            Add(m7);
+            var m8 = new MenuItemModel(8, "Hot Chocolate", true, 15);
+            Add(m8);
+        }
+    }
+}
